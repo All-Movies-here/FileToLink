05e2289f3d207caa70524d940c8a2c1d2c02a0c0
All-Movies-here <sumantsharma119@gmail.com>
2025-03-11 13:17:46 +0530

    Update info.py

--- info.py
+++ info.py
@@ -5,9 +5,9 @@ id_pattern = re.compile(r'^.\d+$')
 
 # Bot information
 SESSION = environ.get('SESSION', 'TechVJBot')
-API_ID = int(environ.get('API_ID', ''))
-API_HASH = environ.get('API_HASH', '')
-BOT_TOKEN = environ.get('BOT_TOKEN', "")
+API_ID = int(environ.get('API_ID', '28735699'))
+API_HASH = environ.get('API_HASH', '2e19c326d8cb322df7c15d7b7e84d1f3')
+BOT_TOKEN = environ.get('BOT_TOKEN', "7476947189:AAGTbDT_iBGoJldOClkz2zlt1Ka0syRkvn8")
 
 # Bot settings
 PORT = environ.get("PORT", "8080")
@@ -23,8 +23,8 @@ else:
 URL = environ.get("URL", "")
 
 # Admins, Channels & Users
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002302355911'))
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6083610190').split()]
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "")
